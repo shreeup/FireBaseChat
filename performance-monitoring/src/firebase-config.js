1f02c101f6633280828ba1740e2a7eea011a6be9
Shree <shree912@yahoo.com>
2022-11-28 16:35:24 -0800

    Push to git

--- performance-monitoring/src/firebase-config.js
+++ performance-monitoring/src/firebase-config.js
@@ -6,8 +6,9 @@
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
  */
+ /** 
  const config = {
-    /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+     TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE 
   };
   
   export function getFirebaseConfig() {
@@ -18,3 +19,38 @@
       return config;
     }
   }
+
+  */
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBfeU-FJQtY54GqutEBWYo06bdl081h1FM",
+  authDomain: "fir-chat-4284a.firebaseapp.com",
+  projectId: "fir-chat-4284a",
+  storageBucket: "fir-chat-4284a.appspot.com",
+  messagingSenderId: "598623757327",
+  appId: "1:598623757327:web:cb0c900d279dd26ff96f7a",
+  measurementId: "G-BKZNM6ZGNN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+export function getFirebaseConfig() {
+  if (!firebaseConfig || !firebaseConfig.apiKey) {
+    throw new Error('No Firebase configuration object provided.' + '\n' +
+    'Add your web app\'s configuration object to firebase-config.js');
+  } else {
+    return firebaseConfig;
+  }
+}
--- web/src/firebase-config.js
+++ web/src/firebase-config.js
@@ -6,9 +6,20 @@
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
  */
+
+
 const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  apiKey: "AIzaSyBfeU-FJQtY54GqutEBWYo06bdl081h1FM",
+  authDomain: "fir-chat-4284a.firebaseapp.com",
+  projectId: "fir-chat-4284a",
+  storageBucket: "fir-chat-4284a.appspot.com",
+  messagingSenderId: "598623757327",
+  appId: "1:598623757327:web:cb0c900d279dd26ff96f7a",
+  measurementId: "G-BKZNM6ZGNN"
 };
+
+const app = initializeApp(config);
+const analytics = getAnalytics(app);
 
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
